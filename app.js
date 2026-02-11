8f974fe4abec8f48c2da923682bbf033dd746413
ARTHUR FAGUNDES TEJADA MOREIRA <arthur.ftmoreira@senacsp.edu.br>
2026-02-10 22:01:30 -0300

    novo

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 console.log("Ola,mundo !")
 const variavel = document.getElementById("root")
-variavel.innerHTML = "Meu primeiro codigo em JavaScript. Já sou programador"
+variavel.innerHTML = "Meu primeiro codigo em JavaScript. Já sou programador, estou com sono"
